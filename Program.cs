1b812a34e92873378c8213907944660f16f17484
Charleu <sdlimeira0@gmail.com>
2024-08-06 19:49:51 -0300

    Resolvido o problema do Palindro

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavrasPalindromos = new string[] { "ovo", "osso", "pop", "ele" };
+            string separador = new string('-', 25);
+            string palavra;
+
+            Console.WriteLine(separador);
+            Console.WriteLine("Palavras Palíndromos");
+            Console.WriteLine(separador);
+            Console.WriteLine("");
+
+            Console.WriteLine("Informe se a palavra é um Palíndromo ou não:");
+            palavra = Console.ReadLine();
+            if (palavrasPalindromos[0] == palavra)
+            {
+                Console.WriteLine("É um palíndromo");
+            }
+            else if (palavrasPalindromos[1] == palavra)
+            {
+                Console.WriteLine("É um palíndromo");
+            }
+            else if (palavrasPalindromos[2] == palavra)
+            {
+                Console.WriteLine("É um palíndromo");
+            }
+            else if (palavrasPalindromos[3] == palavra)
+            {
+                Console.WriteLine("É um palíndromo");
+            }
+            else
+            {
+                Console.WriteLine("Não é um palíndromo");
+            }
+
+        }
+    }
+}
